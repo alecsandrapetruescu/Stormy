78ce4b68020c4a5979128f126ecc12c6d3f4acb8
Alecsandra <alecsandra.petruescu@gmail.com>
2016-08-31 23:17:27 +0300

    use google maps api for latitude and longitude

--- app/src/main/java/com/example/alecsandra/stormy/MainActivity.java
+++ app/src/main/java/com/example/alecsandra/stormy/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.alecsandra.stormy;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +15,12 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,11 +35,17 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
     private CurrentWeather mCurrentWeather;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private double mLatitude;
+    private double mLongitude;
 
     @BindView(R.id.timeLabel) TextView mTimeLabel;
     @BindView(R.id.temperatureLabel) TextView mTemperatureLabel;
@@ -50,27 +65,54 @@ public class MainActivity extends AppCompatActivity {
 
         toggleRefresh();
 
-        final double latitude = 37.8267;
-        final double longitude = -122.423;
-
         mrefreshImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getForecast(latitude, longitude);
+                locationProvider();
+                getForecast(mLatitude, mLongitude);
             }
         });
 
-        getForecast(latitude, longitude);
+        locationProvider();
+        getForecast(mLatitude, mLongitude);
 
         Log.d(TAG, "Main UI code is running");
 
     }
 
+    private void locationProvider() {
+        if (isNetworkAvailable()) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // Create an instance of GoogleAPIClient.
+                    if (mGoogleApiClient == null) {
+                        mGoogleApiClient = new GoogleApiClient.Builder(MainActivity.this)
+                                .addConnectionCallbacks(MainActivity.this)
+                                .addOnConnectionFailedListener(MainActivity.this)
+                                .addApi(LocationServices.API)
+                                .build();
+
+                        mGoogleApiClient.connect();
+                    }
+
+                    // Create the LocationRequest object
+                    mLocationRequest = LocationRequest.create()
+                            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                            .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                            .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+                }
+            });
+
+        } else {
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+        }
+    }
+
     private void getForecast(double latitude, double longitude) {
-        String apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
-        String forecast = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
-
+        String forecast = getResources().getString(R.string.forecast_url) + getResources().getString(R.string.forecast_api_key) + "/" + latitude + "," + longitude;
+        Log.d(TAG, forecast);
         if (isNetworkAvailable()) {
             toggleRefresh();
 
@@ -193,4 +235,44 @@ public class MainActivity extends AppCompatActivity {
         AlertDialogFragment dialog = new AlertDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            Log.d(TAG, "location null");
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        } else {
+            mLatitude = location.getLatitude();
+            mLongitude = location.getLongitude();
+            Log.d(TAG, "Latitude " + mLatitude);
+            Log.d(TAG, "Longitude " + mLongitude);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
 }
