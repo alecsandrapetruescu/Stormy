bdc6780834dc0051765a1a591cbc2640b560a5b4
Alecsandra <alecsandra.petruescu@gmail.com>
2016-09-20 21:04:17 +0300

    display empty view for RecyclerView

--- app/src/main/java/com/example/alecsandra/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/example/alecsandra/stormy/ui/HourlyForecastActivity.java
@@ -6,6 +6,7 @@ import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.alecsandra.stormy.R;
@@ -21,6 +22,7 @@ public class HourlyForecastActivity extends AppCompatActivity {
 
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     @BindView(R.id.hourlyLocationLabel) TextView mLocation;
+    @BindView(android.R.id.empty) TextView mEmptyTextView;
     private Hour[] mHours;
 
     @Override
@@ -34,13 +36,21 @@ public class HourlyForecastActivity extends AppCompatActivity {
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
 
-        HourAdapter adapter = new HourAdapter(this, mHours);
-        mRecyclerView.setAdapter(adapter);
+        if (mHours.length > 0) {
+            mRecyclerView.setVisibility(View.VISIBLE);
+            mEmptyTextView.setVisibility(View.INVISIBLE);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(layoutManager);
+            HourAdapter adapter = new HourAdapter(this, mHours);
+            mRecyclerView.setAdapter(adapter);
 
-        mRecyclerView.setHasFixedSize(true);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            mRecyclerView.setLayoutManager(layoutManager);
+
+            mRecyclerView.setHasFixedSize(true);
+        } else {
+            mRecyclerView.setVisibility(View.INVISIBLE);
+            mEmptyTextView.setVisibility(View.VISIBLE);
+        }
 
         String location = intent.getStringExtra(MainActivity.YOUR_LOCATION);
         mLocation.setText(location);
